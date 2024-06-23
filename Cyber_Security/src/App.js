7bde5a9f5296430e8fd2b21b26d141a7d7602101
ProjectWithRavi <raviyt73@gmail.com>
2024-06-23 17:30:49 +0530

    this is my react project

---
+++ Cyber_Security/src/App.js
@@ -0,0 +1,13 @@
+// import Myrouter from "./components/cybersecurity/myrouter";
+
+import Myrouter from "./components/myrouter";
+
+const App = ()=>{
+  return (
+    // <Myrouter/>
+    
+    <Myrouter/>
+  );
+}
+
+export default App;
